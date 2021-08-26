a3d146a7e871342a8aa0427f1c1320bcafd0adeb
amdevper <68313421+amdevper@users.noreply.github.com>
2021-08-26 22:00:37 +0700

    trial client server

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
-const path = require('path')
-app.get('/',(req,res)=>{
-    res.sendFile("index.html",{
-        root:path.join(__dirname,'./views')
-    })
-})
-    //res.send("<h1>Hello app</h1>")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const path = require("path");
+const socketio = require("socket.io");
+app.get("/", (req, res) => {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "./views"),
+  });
+});
+//res.send("<h1>Hello app</h1>")
+
+const server = app.listen(PORT);
+
+const io = socketio(server);
+io.on("connection", (socket) => {
+  console.log("a client is connect");
+  socket.on("disconnect", () => {
+    console.log("a client is disconnect");
+  });
+  socket.on("srvcli",(message)=>{
+      console.log("Receive from client :",message)
+  })
+  
+});
 
 
-app.listen(PORT)
+
+//io.socket.emit('data1','i send data 1')
+setInterval(function () {
+  var currentDate = new Date();
+
+  io.sockets.emit("servcli1", { currentDate: currentDate });
+  console.log('i send and emit every 1s.')
+}, 10000);
