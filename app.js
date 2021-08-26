b1d797ad361335a814bd18d4425d5f564ebc444c
amdevper <68313421+amdevper@users.noreply.github.com>
2021-08-26 11:54:43 +0700

    npm version up

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 8080
 app.get('/',(req,res)=>{
     res.send("<h1>Hello app</h1>")
 })
 
-app.listen(process.env.PORT||8080)
+app.listen(PORT)
